30b066b5376292c4cd4d1cf26dad11695ab9e5ad
sfnewzgirl <YOUR_GITHUB_EMAIL_ADDRESS>
2016-09-26 17:45:06 -0700

    finishes arguments, all but one

--- starter-code/arguments.rb
+++ starter-code/arguments.rb
@@ -1,24 +1,34 @@
 #########################
 #### USING ARGUMENTS ####
 #########################
-#say_hello
-  # returns 'hello'
+def say_hello
+  'hello'
+end
 
-#echo
-  # returns the input string
+def echo(input)
+  input
+end
 
-#eddie_izzards_height
+def eddie_izzards_height(input=0)
+  67 + input
   # calculates and returns Eddie Izzard's height
   # takes in the height of heels he's wearing (default value: 0)
   # and adds heel height to his base 67 inches for the result
+end
 
-#how_many_args
+def how_many_args(*args)
+  args.count
   # accepts any number of arguments without complaint
   # returns the total number of arguments passed to the function
   # Hint: google ruby *args
+end
 
-#find_answer
+# solution, not my code
+def find_answer(kwargs={})
+  kwargs[:answer]
   # returns the value associated with the 'answer' keyword argument
   # returns nil if it cannot find the 'answer' keyword argument
   # complains when given non-keyword arguments
   # Hint: google ruby keyword arguments
+  # expect { find_answer("bad input") }.to raise_error TypeError
+end
